ececa6d86c2de848692e4e349c7f61d24c21396d
AdrianGniadek <adrian.gniadek03@gmail.com>
2025-03-07 19:50:29 +0100

    First commit

---
+++ src/test/java/com/AdrianGniadek/GithubAPI/GithubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdrianGniadek.GithubAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
